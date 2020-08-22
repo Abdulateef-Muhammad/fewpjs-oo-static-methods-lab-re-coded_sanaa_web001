18ad1b227b786f121ef7f94fb5318d653ece3e34
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-22 06:18:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[^a-zA-Z]/g, '');
+    return string.replace(/[^a-zA-Z/s]/g, '');
   }
 
   static titleize(string) {
