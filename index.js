259d6e49ab8851cc6de6091c076af7fc7001cf7c
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-08-22 06:05:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase().split('').concat(string.slice(1)).join('');
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z]/g, string);
+  }
 }
+
+let soma  = Formatter.sanitize('1soma');
+console.log(soma);
